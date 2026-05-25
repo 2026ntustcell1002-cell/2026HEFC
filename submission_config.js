fbf926afe0a9c677905c3013ea740534b6b01d53
豐豐 <fengfeng@fengfengdeAir.mshome.net>
2026-05-25 11:57:45 +0800

    Publish HEFC 2026 html0512 site

---
+++ submission_config.js
@@ -0,0 +1,5 @@
+window.HEFC_SUBMISSION_API_URL =
+'https://script.google.com/macros/s/AKfycbwFwY4KjCwlGTdrsDxH6h_1mayJvtbnosXrHbn1ebkotUZmeCiWE2yhalhPx9JTdcU0eA/exec';
+
+window.HEFC_GOOGLE_CLIENT_ID =
+'521873320707-kmujg3oqnipuce98gmldhbav024oeddg.apps.googleusercontent.com';
